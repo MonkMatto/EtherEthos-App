3dc56b2c3f814ac839f94741059c484a13fcd91d
tophercully <89924670+tophercully@users.noreply.github.com>
2024-05-23 19:27:30 +0200

    revert contract logic

--- js/handshake.js
+++ js/handshake.js
@@ -8,86 +8,85 @@ const web3Polygon = new Web3(`https://opt-mainnet.g.alchemy.com/v2/${ALCHEMY_POL
 const web3Zora = new Web3(`https://base-mainnet.g.alchemy.com/v2/${ALCHEMY_ZORA}`);
 let currentChain = null;
 let currentAccount = null;
-window.sessionStorage.setItem('chainIDLoaded', false)
+window.sessionStorage.setItem("chainIDLoaded", false);
 
 // blast not yet supported
 let chains = [
   {
-    name: 'mainnet',
+    name: "mainnet",
     id: 1,
     explorerBaseUrl: "https://etherscan.io/address/",
-    contractAddress: "0x1f4126A9D34811E55B9506F011aC1df1396ac909"
+    contractAddress: "0x1f4126A9D34811E55B9506F011aC1df1396ac909",
   },
   {
-    name: 'sepolia',
+    name: "sepolia",
     id: 11155111,
     explorerBaseUrl: "https://sepolia.etherscan.io/address/",
-    contractAddress: "0x96E7F0a77f2272865e431F6e41B41d580AeEa0eb"
+    contractAddress: "0x96E7F0a77f2272865e431F6e41B41d580AeEa0eb",
   },
   {
-    name: 'optimism',
+    name: "optimism",
     id: 10,
     explorerBaseUrl: "https://optimistic.etherscan.io/address/",
-    contractAddress: "0x1f4126A9D34811E55B9506F011aC1df1396ac909"
+    contractAddress: "0x1f4126A9D34811E55B9506F011aC1df1396ac909",
   },
   {
-    name: 'base',
+    name: "base",
     id: 8453,
     explorerBaseUrl: "https://basescan.org/address/",
-    contractAddress: "0x1f4126A9D34811E55B9506F011aC1df1396ac909"
+    contractAddress: "0x1f4126A9D34811E55B9506F011aC1df1396ac909",
   },
   {
-    name: 'arbitrum',
+    name: "arbitrum",
     id: 42161,
     explorerBaseUrl: "https://arbiscan.io/address/",
-    contractAddress: "0x1f4126A9D34811E55B9506F011aC1df1396ac909"
+    contractAddress: "0x1f4126A9D34811E55B9506F011aC1df1396ac909",
   },
   {
-    name: 'polygon',
+    name: "polygon",
     id: 137,
     explorerBaseUrl: "https://polygonscan.com/address/",
-    contractAddress: "0x1f4126A9D34811E55B9506F011aC1df1396ac909"
+    contractAddress: "0x1f4126A9D34811E55B9506F011aC1df1396ac909",
   },
   {
-    name: 'zora',
+    name: "zora",
     id: 7777777,
     explorerBaseUrl: "https://zora.superscan.network/address/",
-    contractAddress: "0x1f4126A9D34811E55B9506F011aC1df1396ac909"
+    contractAddress: "0x1f4126A9D34811E55B9506F011aC1df1396ac909",
   },
   {
-    name: 'blast',
+    name: "blast",
     id: 81457,
     explorerBaseUrl: "https://blastexplorer.io/address/",
-    contractAddress: "0x1f4126A9D34811E55B9506F011aC1df1396ac909"
+    contractAddress: "0x1f4126A9D34811E55B9506F011aC1df1396ac909",
   },
-]
+];
 
-var chainParams = new URLSearchParams(window.location.search)
-let EE_ADDRESS
-// if(chainParams.has("chain")) {
-//   //Find contract address based on search query
-//   var findChainID = chains.find(o => o.id === Number(chainParams.get("chain")))
-//   chainIndex = chains.indexOf(findChainID);
-//   EE_ADDRESS = chains[chainIndex].contractAddress
-// } else {
-//   console.log('trying to use network from metamask')
-//   if(currentAccount) {
-//     //Find contract address based on wallet chainID
-//     window.addEventListener('load', ()=>{
-//       var findChainID = chains.find(o => o.id === Number(chainParams.get("chain")))
-//       chainIndex = chains.indexOf(findChainID);
-//       EE_ADDRESS = chains[chainIndex].contractAddress
-      
-//     })
-//     console.log('successfully used network from metamask, ' + chains[chainIndex].name)
-//   } else {
-//     //Use default (mainnet)
-//     console.log('defaulted to mainnet')
-//     createStatusMsg('defaulted to mainnet')
-//     chainIndex = 0
-//     EE_ADDRESS = "0x1f4126A9D34811E55B9506F011aC1df1396ac909";
-//   }
-// }
+var chainParams = new URLSearchParams(window.location.search);
+let EE_ADDRESS;
+if (chainParams.has("chain")) {
+  //Find contract address based on search query
+  var findChainID = chains.find((o) => o.id === Number(chainParams.get("chain")));
+  chainIndex = chains.indexOf(findChainID);
+  EE_ADDRESS = chains[chainIndex].contractAddress;
+} else {
+  console.log("trying to use network from metamask");
+  if (currentAccount) {
+    //Find contract address based on wallet chainID
+    window.addEventListener("load", () => {
+      var findChainID = chains.find((o) => o.id === Number(chainParams.get("chain")));
+      chainIndex = chains.indexOf(findChainID);
+      EE_ADDRESS = chains[chainIndex].contractAddress;
+    });
+    console.log("successfully used network from metamask, " + chains[chainIndex].name);
+  } else {
+    //Use default (mainnet)
+    console.log("defaulted to mainnet");
+    createStatusMsg("defaulted to mainnet");
+    chainIndex = 0;
+    EE_ADDRESS = "0x1f4126A9D34811E55B9506F011aC1df1396ac909";
+  }
+}
 const EE_Contract_Alchemy = new web3Main.eth.Contract(EE_ABI, chains[0].contractAddress);
 const EE_Contract_Alchemy_Sepolia = new web3Sepolia.eth.Contract(EE_ABI, chains[1].contractAddress);
 const EE_Contract_Alchemy_Optimism = new web3Optimism.eth.Contract(EE_ABI, chains[2].contractAddress);
@@ -111,7 +110,7 @@ async function getProvider() {
     startApp(provider);
   } else {
     console.log("Please install MetaMask!");
-    createNoWalletMsg()
+    createNoWalletMsg();
   }
 }
 
@@ -120,7 +119,7 @@ getProvider();
 function startApp(provider) {
   if (provider !== window.ethereum) {
     console.error("Do you have multiple wallets installed?");
-    createErrorMsg("Error! Do you have multiple wallets installed?")
+    createErrorMsg("Error! Do you have multiple wallets installed?");
   }
   web3User = new Web3(provider); // Initialize user's web3
 
@@ -138,7 +137,7 @@ function startApp(provider) {
       // Convert the chain ID from hex to decimal
       currentChainId = parseInt(chainId, 16);
       console.log("Current Chain ID:", currentChainId);
-      window.sessionStorage.setItem('chainIDLoaded', true)
+      window.sessionStorage.setItem("chainIDLoaded", true);
       // You can now use currentChainId in other parts of your app
     })
     .catch(handleError);
@@ -154,109 +153,108 @@ function handleChainChanged() {
   window.location.reload();
 }
 
-
-
 function handleAccountsChanged(accounts) {
   if (accounts.length === 0) {
     console.log("Please connect to MetaMask.");
     connectButtonText.textContent = "Connect MetaMask";
   } else if (accounts[0] !== currentAccount) {
-    const firstConnection = currentAccount ? false : true
-    console.log('setting currentAccount')
+    const firstConnection = currentAccount ? false : true;
+    console.log("setting currentAccount");
     currentAccount = accounts[0];
     console.log("Account connected:", currentAccount);
     console.log(currentAccount);
-    connectButtonText.textContent = typeof currentAccount == 'string' ? `${currentAccount.slice(0, 6)}...${currentAccount.slice(-4)}` : currentAccount;
-
+    connectButtonText.textContent = typeof currentAccount == "string" ? `${currentAccount.slice(0, 6)}...${currentAccount.slice(-4)}` : currentAccount;
   }
 }
 
 function handleError(error) {
   console.error(error);
-  if(!currentChainId) {
-    currentChainId = 1
+  if (!currentChainId) {
+    currentChainId = 1;
   }
 }
 
 function connectWallet() {
-  if(!currentAccount) {
+  if (!currentAccount) {
     //initial connection
-    ethereum.request({ method: "eth_requestAccounts" }).then(!currentAccount ? handleAccountsChanged : ()=> {}).catch(handleError);
+    ethereum
+      .request({ method: "eth_requestAccounts" })
+      .then(!currentAccount ? handleAccountsChanged : () => {})
+      .catch(handleError);
   } else {
     //connect new account
-    ethereum.request({
-      method: "wallet_requestPermissions",
-      params: [
-        {
-          eth_accounts: {}
-        }
-      ]
-    }).then(handleAccountsChanged).catch(handleError);
-    console.log('reconnecting')
+    ethereum
+      .request({
+        method: "wallet_requestPermissions",
+        params: [
+          {
+            eth_accounts: {},
+          },
+        ],
+      })
+      .then(handleAccountsChanged)
+      .catch(handleError);
+    console.log("reconnecting");
   }
 }
 
-function manageDropdown () {
+function manageDropdown() {
   if (connectButton) {
-    if(!currentAccount) {
-      const profileDropdown = document.getElementById('profile-dropdown')
-      profileDropdown.classList.remove('flex')
-      profileDropdown.classList.add('hidden')
-      profileDropdown.style.border = 'none'
+    if (!currentAccount) {
+      const profileDropdown = document.getElementById("profile-dropdown");
+      profileDropdown.classList.remove("flex");
+      profileDropdown.classList.add("hidden");
+      profileDropdown.style.border = "none";
 
       connectButton.addEventListener("click", function (event) {
         event.preventDefault();
         connectWallet();
       });
-    } else if(currentAccount) {
-      removeEventListener('click', connectButton)
-      let profileDropped = false
-      const profileDropdown = document.getElementById('profile-dropdown')
-      profileDropdown.style.border = 'none'
-      const myProfileButton = document.getElementById('my-profile')
-      myProfileButton.style.height = '0'
-      const reconnectButton = document.getElementById('reconnect')
-      reconnectButton.style.height = '0'
-
+    } else if (currentAccount) {
+      removeEventListener("click", connectButton);
+      let profileDropped = false;
+      const profileDropdown = document.getElementById("profile-dropdown");
+      profileDropdown.style.border = "none";
+      const myProfileButton = document.getElementById("my-profile");
+      myProfileButton.style.height = "0";
+      const reconnectButton = document.getElementById("reconnect");
+      reconnectButton.style.height = "0";
 
       function toggleProfileDropdown() {
-        if(profileDropped) {
-          profileDropdown.style.height = '0px'
-          myProfileButton.style.height = '0px'
-          reconnectButton.style.height = '0px'
-          profileDropdown.style.border = 'none'
-          profileDropdown.classList.add('hidden')
-          profileDropped = false
-          
+        if (profileDropped) {
+          profileDropdown.style.height = "0px";
+          myProfileButton.style.height = "0px";
+          reconnectButton.style.height = "0px";
+          profileDropdown.style.border = "none";
+          profileDropdown.classList.add("hidden");
+          profileDropped = false;
         } else {
-          profileDropdown.style.height = '10rem'
-          myProfileButton.style.height = '5rem'
-          reconnectButton.style.height = '5rem'
-          profileDropdown.style.border = '1px solid black'
-          profileDropdown.classList.remove('hidden')
-          profileDropped = true
+          profileDropdown.style.height = "10rem";
+          myProfileButton.style.height = "5rem";
+          reconnectButton.style.height = "5rem";
+          profileDropdown.style.border = "1px solid black";
+          profileDropdown.classList.remove("hidden");
+          profileDropped = true;
         }
       }
-      
+
       connectButton.addEventListener("click", function (event) {
         event.preventDefault();
         toggleProfileDropdown();
       });
-      reconnectButton.addEventListener('click', ()=>{
+      reconnectButton.addEventListener("click", () => {
         connectWallet();
-      })
-      myProfileButton.addEventListener('click', ()=>{
+      });
+      myProfileButton.addEventListener("click", () => {
+        window.location.href = `${window.location.origin}?address=${currentAccount}`;
 
-        window.location.href = `${window.location.origin}?address=${currentAccount}`
-
-        console.log('my profile link ' + profileLink)
-        console.log(profileParams)
-      })
+        console.log("my profile link " + profileLink);
+        console.log(profileParams);
+      });
     }
   }
 }
 
-window.addEventListener('load', ()=> {
-  manageDropdown()
-}) 
-  
+window.addEventListener("load", () => {
+  manageDropdown();
+});
