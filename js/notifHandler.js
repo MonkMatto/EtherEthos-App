da7afab22900a17024ec8e3cf9fbf3ef2cd60265
tophercully <89924670+tophercully@users.noreply.github.com>
2024-06-21 18:50:58 +0200

    fix verification response call

--- js/notifHandler.js
+++ js/notifHandler.js
@@ -1,113 +1,113 @@
 const createErrorMsg = (msg) => {
-    //check for existing messages
-    var existingMsg = document.getElementById('wallet-status-msg')
-    if(existingMsg) {
-        existingMsg.remove()
-    }
-    //build message module
-    var errorBody = document.createElement('div')
-    var errorMsg = document.createElement('h1')
-    errorBody.setAttribute('id', "wallet-status-msg")
-    var closeButton = document.createElement('button')
-    
-    //style
-    errorBody.className = 'fixed bottom-0 left-0 w-full h-12 z-100 px-5 flex items-center justify-between bg-red'
-    errorMsg.setAttribute('class', 'text-base')
+  //check for existing messages
+  var existingMsg = document.getElementById("wallet-status-msg");
+  if (existingMsg) {
+    existingMsg.remove();
+  }
+  //build message module
+  var errorBody = document.createElement("div");
+  var errorMsg = document.createElement("h1");
+  errorBody.setAttribute("id", "wallet-status-msg");
+  var closeButton = document.createElement("button");
 
-    closeButton.setAttribute('class', 'w-1/10 aspect-square bg-red-400')
-    
-    //fill message text
-    if(msg) {
-        errorMsg.innerHTML = msg
+  //style
+  errorBody.className =
+    "fixed bottom-0 left-0 w-full h-12 z-100 px-5 flex items-center justify-between bg-red-400";
+  errorMsg.setAttribute("class", "text-base");
 
-    } else {
-        errorMsg.innerHTML = 'Transaction Error'
-    }
-    errorBody.appendChild(errorMsg)
-    
-    //button event
-    closeButton.innerHTML = 'Close'
-    closeButton.addEventListener('click', ()=>{
-        errorBody.remove()
-    })
-    errorBody.appendChild(closeButton)
+  closeButton.setAttribute("class", "w-1/10 aspect-square bg-red-400");
 
-    //attach to document
-    document.body.appendChild(errorBody)
-}
+  //fill message text
+  if (msg) {
+    errorMsg.innerHTML = msg;
+  } else {
+    errorMsg.innerHTML = "Transaction Error";
+  }
+  errorBody.appendChild(errorMsg);
+
+  //button event
+  closeButton.innerHTML = "Close";
+  closeButton.addEventListener("click", () => {
+    errorBody.remove();
+  });
+  errorBody.appendChild(closeButton);
+
+  //attach to document
+  document.body.appendChild(errorBody);
+};
 
 const createStatusMsg = (msg) => {
-    //check for existing messages
-    var existingMsg = document.getElementById('wallet-status-msg')
-    if(existingMsg) {
-        existingMsg.remove()
-    }
-    //build message module
-    var statusBody = document.createElement('div')
-    var statusMsg = document.createElement('h1')
-    statusBody.setAttribute('id', "wallet-status-msg")
-    var closeButton = document.createElement('button')
-    
-    //style
-    statusBody.className = 'fixed bottom-0 left-0 w-full h-12 z-100 px-5 flex items-center justify-between bg-blue'
+  //check for existing messages
+  var existingMsg = document.getElementById("wallet-status-msg");
+  if (existingMsg) {
+    existingMsg.remove();
+  }
+  //build message module
+  var statusBody = document.createElement("div");
+  var statusMsg = document.createElement("h1");
+  statusBody.setAttribute("id", "wallet-status-msg");
+  var closeButton = document.createElement("button");
 
-    // statusMsg.setAttribute('class', '')
-    closeButton.setAttribute('class', 'w-1/10 aspect-square bg-red-400')
-    
-    //fill message text
-    if(!msg) {
-        statusMsg.innerHTML = 'Sending transaction to wallet...'
-        
-    } else {
-        
-        statusMsg.innerHTML = msg
-    }
-    statusBody.appendChild(statusMsg)
-    
-    //button event
-    closeButton.innerHTML = 'Close'
-    closeButton.addEventListener('click', ()=>{
-        statusBody.remove()
-    })
-    statusBody.appendChild(closeButton)
+  //style
+  statusBody.className =
+    "fixed bottom-0 left-0 w-full h-12 z-100 px-5 flex items-center justify-between bg-blue";
 
-    //attach to document
-    document.body.appendChild(statusBody)
+  // statusMsg.setAttribute('class', '')
+  closeButton.setAttribute("class", "w-1/10 aspect-square bg-red-400");
 
-    setTimeout(()=>{
-        statusBody.remove()
-    }, 10000)
-}
+  //fill message text
+  if (!msg) {
+    statusMsg.innerHTML = "Sending transaction to wallet...";
+  } else {
+    statusMsg.innerHTML = msg;
+  }
+  statusBody.appendChild(statusMsg);
+
+  //button event
+  closeButton.innerHTML = "Close";
+  closeButton.addEventListener("click", () => {
+    statusBody.remove();
+  });
+  statusBody.appendChild(closeButton);
+
+  //attach to document
+  document.body.appendChild(statusBody);
+
+  setTimeout(() => {
+    statusBody.remove();
+  }, 10000);
+};
 
 const createNoWalletMsg = (msg) => {
-    //check for existing messages
-    var existingMsg = document.getElementById('wallet-status-msg')
-    if(existingMsg) {
-        existingMsg.remove()
-    }
-    //build message module
-    var statusBody = document.createElement('div')
-    var statusMsg = document.createElement('h1')
-    statusBody.setAttribute('id', "wallet-status-msg")
-    var closeButton = document.createElement('button')
-    
-    //style
-    statusBody.className = 'bg-red-400 fixed top-half left-0 w-full h-28 z-100 px-5 flex items-center justify-between'
+  //check for existing messages
+  var existingMsg = document.getElementById("wallet-status-msg");
+  if (existingMsg) {
+    existingMsg.remove();
+  }
+  //build message module
+  var statusBody = document.createElement("div");
+  var statusMsg = document.createElement("h1");
+  statusBody.setAttribute("id", "wallet-status-msg");
+  var closeButton = document.createElement("button");
 
-    // statusMsg.setAttribute('class', '')
-    closeButton.setAttribute('class', 'w-1/10 aspect-square bg-red-400')
-    
-    //fill message text
-    statusMsg.innerHTML = 'Please install Metamask!'
-    statusBody.appendChild(statusMsg)
-    
-    //button event
-    closeButton.innerHTML = 'Close'
-    closeButton.addEventListener('click', ()=>{
-        statusBody.remove()
-    })
-    statusBody.appendChild(closeButton)
+  //style
+  statusBody.className =
+    "bg-red-400 fixed top-half left-0 w-full h-28 z-100 px-5 flex items-center justify-between";
 
-    //attach to document
-    document.body.appendChild(statusBody)
-}
+  // statusMsg.setAttribute('class', '')
+  closeButton.setAttribute("class", "w-1/10 aspect-square bg-red-400");
+
+  //fill message text
+  statusMsg.innerHTML = "Please install Metamask!";
+  statusBody.appendChild(statusMsg);
+
+  //button event
+  closeButton.innerHTML = "Close";
+  closeButton.addEventListener("click", () => {
+    statusBody.remove();
+  });
+  statusBody.appendChild(closeButton);
+
+  //attach to document
+  document.body.appendChild(statusBody);
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}", "./dist/*.{html,js}"],
+  mode: "jit",
+  content: [
+    "./*.{html,js}",
+    "./dist/*.{html,js}",
+    "./js/*.js",
+    "!./js/web3.min.js",
+  ],
   theme: {
-    colors: { main: "#364659", secondary: "#fffcf9", blue: "#DAECFC", orange: "#FCECDA" },
+    colors: {
+      main: "#364659",
+      secondary: "#fffcf9",
+      blue: "#DAECFC",
+      orange: "#FCECDA",
+      error: "#ff7272",
+    },
   },
   plugins: [],
 };
